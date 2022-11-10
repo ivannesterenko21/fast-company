3b8ef8ecb766fa889022ed08145c9c110bb963b0
Ivan Nesterenko <nester164ivan@yandex.ru>
2022-11-10 23:24:28 +0300

    added pagination at the bottom of the page and type checking, set up ESlint and Prettier

--- src/app/components/user.jsx
+++ src/app/components/user.jsx
@@ -1,42 +1,56 @@
-import React from 'react';
-import Qualitie from './qualitie';
-import BookMark from './bookmark';
-
-const User = ({ user, onDelete, onClick, ...rest }) => {
-  return (
-    <tr key={user._id}>
-      <td>{user.name}</td>
-      <td>
-        {user.qualities.map((qualitie) => {
-          return (
-            <Qualitie
-              key={qualitie._id}
-              color={qualitie.color}
-              name={qualitie.name}
-            />
-          );
-        })}
-      </td>
-      <td>{user.profession.name}</td>
-      <td>{user.completedMeetings}</td>
-      <td>{user.rate}</td>
-      <td>
-        <BookMark
-          bookmark={user.bookmark}
-          userId={user._id}
-          onClick={onClick}
-        />
-      </td>
-      <td>
-        <button
-          className="btn btn-danger btn-sm m-2"
-          onClick={() => onDelete(user._id)}
-        >
-          delete
-        </button>
-      </td>
-    </tr>
-  );
+import React from "react";
+import PropTypes from "prop-types";
+import Quality from "./qualitie";
+import BookMark from "./bookmark";
+const User = ({
+    _id,
+    name,
+    qualities,
+    profession,
+    completedMeetings,
+    rate,
+    onDelete,
+    bookmark,
+    onToggleBookMark
+}) => {
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>
+                {qualities.map((qual) => (
+                    <Quality {...qual} key={qual._id} />
+                ))}
+            </td>
+            <td>{profession.name}</td>
+            <td>{completedMeetings}</td>
+            <td>{rate} /5</td>
+            <td>
+                <BookMark
+                    status={bookmark}
+                    onClick={() => onToggleBookMark(_id)}
+                />
+            </td>
+            <td>
+                <button
+                    onClick={() => onDelete(_id)}
+                    className="btn btn-danger"
+                >
+                    delete
+                </button>
+            </td>
+        </tr>
+    );
+};
+User.propTypes = {
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    qualities: PropTypes.array,
+    profession: PropTypes.object.isRequired,
+    completedMeetings: PropTypes.number.isRequired,
+    rate: PropTypes.number.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    bookmark: PropTypes.bool,
+    onToggleBookMark: PropTypes.func.isRequired
 };
 
 export default User;
